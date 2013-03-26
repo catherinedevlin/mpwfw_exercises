0163fda4f863b47b9fe9728d5aaadfb6804a16b8
Catherine Devlin <catherine_devlin@kace.com>
2013-03-25 23:12:49 -0400

    adding windows install script

---
+++ windows/install.bat
@@ -0,0 +1,10 @@
+python-2.7.3.msi
+setx /M PATH %PATH%;C:\Python27;C:\Python27\Scripts;C:\Python27\Tools\Scripts
+cd distribute-0.6.35
+c:\Python27\python setup.py install
+cd ..
+c:\Python27\Scripts\easy_install pip
+c:\Python27\Scripts\easy_install pyzmq
+c:\Python27\Scripts\pip install pyreadline ipython tornado ipython_doctester
+npp.6.3.Installer.exe
+
