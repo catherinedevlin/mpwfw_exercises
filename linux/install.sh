d988dcc8eb9186bf59848e65c51e6d6a77c09f2c
Catherine Devlin <catherine.devlin@gmail.com>
2013-09-24 01:13:39 -0400

    Switched setup instructions to using Anaconda

--- linux/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if [[ "`which apt-get`" ]]
-then
-  sudo apt-get install python-dev python-setuptools
-else
-  sudo yum install python-devel python-setuptools
-vi
-easy_install pip 
-pip install pyzmq pyreadline ipython tornado ipython_doctester
--- mac/install.sh
+++
@@ -1,2 +0,0 @@
-easy_install pip 
-pip install pyzmq pyreadline ipython tornado ipython_doctester
